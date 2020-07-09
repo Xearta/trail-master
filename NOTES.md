03dee8c29723fdaa4d4a435829c48562a04afb48
Xearta <rsmcwow@gmail.com>
2020-07-09 14:06:39 -0400

    Moved Trail to it's own class

--- NOTES.md
+++ NOTES.md
@@ -26,7 +26,7 @@
 
 [X] The JavaScript application must use Object Oriented JavaScript (classes) to encapsulate related data and behavior.
 
-[X] The domain model served by the Rails backend must include a resource with at least one has-many relationship. For example, if you were building an Instagram clone, you might display a list of photos with associated comments.
+[ ] The domain model served by the Rails backend must include a resource with at least **one has-many relationship**. For example, if you were building an Instagram clone, you might display a list of photos with associated comments.
 
 [X] The backend and frontend must collaborate to demonstrate Client-Server Communication. Your application should have at least **3** AJAX calls, 
 covering **at least 2 of Create, Read, Update, and Delete (CRUD)**. Your client-side JavaScript code must use fetch with the appropriate HTTP verb, and your Rails API should use RESTful conventions.
---
+++ trail-master-frontend/src/trail.js
@@ -0,0 +1,37 @@
+/*
+**** Trail Class ****
+*/
+class Trail {
+    static all = [];
+    
+    constructor(id, name, location, difficulty, completion_time, elevation_gain) {
+        this.id = id;
+        this.name = name
+        this.location = location
+        this.difficulty = difficulty
+        this.completion_time = completion_time
+        this.elevation_gain = elevation_gain
+        Trail.all.push(this);
+    }
+
+    render() {
+        return `
+        <li>
+        <a href="#" data-id="${this.id}">${this.name}</a>
+        - ${this.location}
+        - ${this.difficulty}
+        - ${this.completion_time}
+        - ${this.elevation_gain}
+        - <button id="update" data-id="${this.id}">Edit</button>
+        - <button id="delete" data-id="${this.id}">Delete</button>
+        `
+    }
+
+    static listTrails() {
+        const main = document.querySelector("#main");
+        main.innerHTML = "";
+
+        
+        Trail.all.forEach(trail => main.innerHTML += trail.render())
+    }
+}
